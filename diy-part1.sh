e3b167b250551f5f65edc390f761573652947031
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2024-03-08 13:11:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 #echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git natelol https://github.com/natelol/natelol.git' >>feeds.conf.default
